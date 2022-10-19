c84a3249eedc514d0ed55d150638e99bfeff1796
chethanalb <108932412+chethanalb@users.noreply.github.com>
2022-10-19 15:40:06 +0300

    Update postsAPI.cy.js

--- cypress/e2e/api_tests/postsAPI.cy.js
+++ cypress/e2e/api_tests/postsAPI.cy.js
@@ -5,8 +5,34 @@ const baseURL = 'https://gorest.co.in';
 
 describe('validate post APIs', () =>{
 
-let token = 'Bearer b956717a3a60540f0bf2159475442dee446825c6e5e7cb8b1db3c33c69438a3d'
-let postId = 'abc'
+let token = 'Bearer db1ac56dd0413424eab9763b469e75b3803722f232e8eebb9b1c961a68672066'
+let postId = ''
+let userId = ''
+
+it('create a user to use in Posts data - POST', () =>{
+    var chars = 'abcdefghijklmnopqrstuvwxyz1234567890';
+    var string = '';
+    for(var ii=0; ii<15; ii++){
+         string += chars[Math.floor(Math.random() * chars.length)];
+    }
+    let email = string + '@domain.com';
+
+    cy.request({
+    method: 'POST',
+    url: baseURL+'/public/v2/users',
+    headers: {
+        'Authorization': token
+    },
+    body:{
+        "name": "Chethana",
+        "email": email,
+        "gender": "female",
+        "status": "active"
+    } }).then(function(response){
+        expect(response.status).to.equals(201)
+        userId = response.body['id']
+    })
+})
 
 it('create post data - POST', () =>{
 
@@ -17,7 +43,7 @@ it('create post data - POST', () =>{
         'Authorization': token
     },
     body:{
-        "user_id":"3087",
+        "user_id":userId,
         "title": "Sample Title",
         "body": "Sample Body"
     } }).then(function(response){
@@ -35,7 +61,7 @@ it('retreive post data - GET', ()=>{
     }).then(function(response){
         expect(response.body).have.property('title')
         expect(response.status).to.equals(200)
-        expect(response.duration).to.lessThan(500)
+        expect(response.duration).to.lessThan(1000)
         expect(response.body).property('title').to.equals('Sample Title')
         expect(response.body).property('id').to.equals(postId)
     })
@@ -50,7 +76,7 @@ it('delete post data - DELETE', ()=>{
         url: 'https://gorest.co.in/public/v2/posts/'+postId
     }).then(function(response){
         expect(response.status).to.equals(204)
-        expect(response.duration).to.lessThan(500)
+        expect(response.duration).to.lessThan(1000)
     })
 })
 
